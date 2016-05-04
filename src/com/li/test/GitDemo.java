01a00019690d511b65d122eebb0b30f359a7674b
liqiang <qiangli_i697@anjuke.com>
2016-05-04 16:24:04 +0800

    测试1

---
+++ src/com/li/test/GitDemo.java
@@ -0,0 +1,12 @@
+package com.li.test;
+
+public class GitDemo {
+
+	public static String show(String word){
+		return word;
+	}
+	
+	public static void main(String[] args){
+		System.out.println(show("hello world!"));
+	}
+}
