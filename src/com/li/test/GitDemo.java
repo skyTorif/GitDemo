66188d517dc584ad77c53f877ae7d543a5fc0128
liqiang <qiangli_i697@anjuke.com>
2016-06-22 18:27:37 +0800

    add

--- src/com/li/test/GitDemo.java
+++ src/com/li/test/GitDemo.java
@@ -6,13 +6,13 @@ public class GitDemo {
 		System.out.println("hello world!");
 		System.out.println("5-25");
 	}
-<<<<<<< Updated upstream
+
 	public static String show(String str){
 		return str;
-=======
+	}
+
 	public static int count(int a){
 		return a;
->>>>>>> Stashed changes
 	}
 	//主函数
 	public static void main(String[] args){
